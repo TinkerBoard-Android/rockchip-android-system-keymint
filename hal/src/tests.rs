ae462852f46dd978793a08c2d299378d25652972
Hasini Gunasinghe <hasinitg@google.com>
2022-10-13 21:16:52 +0000

    Add size limit to the channel and processing of larger responses from TA
    Bug: 253501976
    Test: TBA
    Change-Id: I4c186f6b5ed9f51a610b23a6d35167da28c645e1

--- hal/src/tests.rs
+++ hal/src/tests.rs
@@ -3,6 +3,7 @@ use crate::{
     binder,
     hal::keymint::{ErrorCode::ErrorCode, IKeyMintDevice::IKeyMintDevice},
 };
+use kmr_wire::keymint::{NEXT_MESSAGE_SIGNAL_FALSE, NEXT_MESSAGE_SIGNAL_TRUE};
 use std::sync::{Arc, Mutex};
 
 #[derive(Clone, Debug)]
@@ -21,6 +22,7 @@ impl TestChannel {
 }
 
 impl SerializedChannel for TestChannel {
+    const MAX_SIZE: usize = 4096;
     fn execute(&mut self, serialized_req: &[u8]) -> binder::Result<Vec<u8>> {
         *self.req.lock().unwrap() = serialized_req.to_vec();
         Ok(self.rsp.clone())
@@ -83,4 +85,42 @@ fn test_method_err_roundtrip() {
     let status = result.unwrap_err();
     assert_eq!(status.exception_code(), binder::ExceptionCode::SERVICE_SPECIFIC);
     assert_eq!(status.service_specific_error(), ErrorCode::UNSUPPORTED_PURPOSE.0);
+}
+
+#[test]
+fn test_extract_rsp_true_marker() {
+    let msg_content = vec![0x82, 0x21, 0x80];
+    // test true marker and message content
+    let mut resp = vec![NEXT_MESSAGE_SIGNAL_TRUE];
+    resp.extend_from_slice(&msg_content);
+    assert_eq!(Ok((true, msg_content.as_slice())), extract_rsp(&resp));
+}
+
+#[test]
+fn test_extract_rsp_false_marker() {
+    let msg_content = vec![0x82, 0x21, 0x80];
+    // test false signal and message content
+    let mut resp = vec![NEXT_MESSAGE_SIGNAL_FALSE];
+    resp.extend_from_slice(&msg_content);
+    assert_eq!(Ok((false, msg_content.as_slice())), extract_rsp(&resp));
+}
+
+#[test]
+fn test_extract_rsp_empty_input() {
+    // test invalid (empty) input
+    let resp3 = vec![];
+    let result = extract_rsp(&resp3);
+    assert!(result.is_err());
+    let status = result.unwrap_err();
+    assert_eq!(status.exception_code(), binder::ExceptionCode::ILLEGAL_ARGUMENT);
+}
+
+#[test]
+fn test_extract_rsp_single_byte_input() {
+    // test invalid (single byte) input
+    let resp4 = vec![NEXT_MESSAGE_SIGNAL_FALSE];
+    let result = extract_rsp(&resp4);
+    assert!(result.is_err());
+    let status = result.unwrap_err();
+    assert_eq!(status.exception_code(), binder::ExceptionCode::ILLEGAL_ARGUMENT);
 }
